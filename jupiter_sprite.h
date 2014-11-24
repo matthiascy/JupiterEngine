df3deb356756d25ef4d2d0d707ce6d4b8c3dbf0b
matthiascy <matthias_cy@outlook.com>
2014-11-24 19:15:33 +0800

    jupiter sprite complete

--- jupiter_sprite.h
+++ jupiter_sprite.h
@@ -33,7 +33,48 @@ protected:
     int spriteZOrder;
 
 public:
-    
+    JupiterSprite();
+    JupiterSprite(HINSTANCE hInstance, LPTSTR szFileName);
+    Sprite(HINSTANCE hInstance, UINT uRcID);
+    Sprite(HDC hDC, int nWidth, int nHeight, 
+        COLORREF crColor = RGB(255, 255, 255));
+    virtual ~JupiterSprite();
+
+    int getID() { return spriteID; };
+    HBITMAP getBitmap() { return spriteBmp; };
+    void setBitmap(HBITMAP hbm)
+    {
+        if (spriteBmp != NULL)
+            release();
+        spriteBmp = hbm;
+    }
+
+    int getHeight() { return spriteHeight; };
+    int getWidth() { return spriteWidth; };
+    void setZOrder(int nZOrder) { spriteZOrder = nZOrder; };
+    int getZOrder() { return spriteZOrder; };
+
+    void release();
+    bool create(HDC, int nWidth, int nHeight, 
+        COLORREF crColor = RGB(0, 0, 0));
+    bool load(HINSTANCE hInstance, LPTSTR szFileName);
+    bool load(HINSTANCE hInstance, UINT uRcID);
+    void draw(HDC hDC, int x, int y, bool bTrans = false,
+        COLORREF crTransparent = RGB(0, 0, 0));
+    void drawRect(HDC hDC, int x, int y, int nRectX, int nRectY,
+        int nRectWidth, int nRectHeight, bool bTrans = false,
+        COLORREF crTrans = RGB(0, 0, 0));
+    void drawAlpha(HDC hDC, int x, int y, int nAlpha);
+    void drawGray(HDC hDC, int x, int y, bool bTrans = false,
+        COLORREF crTransparent = RGB(0, 0, 0));
+    void drawTransAlpha(HDC hdcBack, int nXBack, int nYBack,
+        int nWidthBack, int nHeightBack, int nXFore, int nYFore,
+        int nAlpha = 100, COLORREF crTrans = RGB(0, 0, 0));
+
+private:
+    void TransBitmap(HDC hdcDest, int nXDest, int nYDest, 
+        int nWidthDest, int nHeightDest, HDC hdcSrc, int nXSrc, int nYSrc,
+        int nWidthSrc, int nHeightSrc, COLORREF crTrans = RGB(255, 255, 255));
 };
 
 #endif // __JUPITER_SPRITE_H__
